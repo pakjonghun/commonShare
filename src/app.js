e7e06a4740a5f1c6c747c842a99f4e04a6ff0fe8
정훈박 <fireking5997@gmail.com>
2021-07-22 21:04:16 +0900

    allow whitelist added

--- src/app.js
+++ src/app.js
@@ -13,14 +13,24 @@ import {
 } from "./api";
 import client from "./client";
 import cors from "cors";
+import { isRef } from "joi";
 
 const app = express();
 
+const whiteList = [
+  "http://localhost:3000",
+  "https://beskinrobbins.s3-website.ap-northeast-2.amazonaws.com",
+];
+
 const corsOptions = {
-  origin: [
-    "http://localhost:3000",
-    "https://beskinrobbins.s3-website.ap-northeast-2.amazonaws.com",
-  ],
+  origin: function (origin, callback) {
+    console.log(origin, callback);
+    if (whiteList.indexOf(origin) >= 0 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("당신은 접근이 허용되지 않았습니다."));
+    }
+  },
 };
 
 app.use(cors(corsOptions));
